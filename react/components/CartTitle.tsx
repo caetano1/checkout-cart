16cdfdaa8510cd12d8c81209086e8a0e7e56bf11
Marcos Kawakami <marcos.kawakami@vtex.com.br>
2019-08-26 12:01:32 -0300

    Translate cart title

--- react/components/CartTitle.tsx
+++ react/components/CartTitle.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react'
+import { FormattedMessage } from 'react-intl'
 
 interface Props {
   items: Item[]
@@ -8,9 +9,15 @@ const CartTitle: FunctionComponent<Props> = ({ items }) => {
   return (
     <div>
       <h3 className="mh5 mh6-m">
-        <span className="t-heading-3 c-on-base t-heading-2-l">Cart</span>
+        <span className="t-heading-3 c-on-base t-heading-2-l">
+          <FormattedMessage id="store/cart.title" />
+        </span>
         <span className="t-heading-5 c-muted-1 t-heading-4-l">
-          &nbsp;({items.length} items)
+          &nbsp;
+          <FormattedMessage
+            id="store/cart.items"
+            values={{ itemsQuantity: items.length }}
+          />
         </span>
       </h3>
     </div>
