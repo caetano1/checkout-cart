3271eb46fcdff1e7a2cc44fbc7a5f33a7001c2bd
Jefferson <jefiim.ad@gmail.com>
2019-11-08 14:02:27 -0300

    Add the needed coupon info as props

--- react/Summary.tsx
+++ react/Summary.tsx
@@ -1,19 +1,40 @@
 import React, { FunctionComponent } from 'react'
 import { ExtensionPoint } from 'vtex.render-runtime'
 import { useOrderForm } from 'vtex.order-manager/OrderForm'
+import {
+  OrderCouponProvider,
+  useOrderCoupon,
+} from 'vtex.order-coupon/OrderCoupon'
 
 const SummaryWrapper: FunctionComponent = () => {
   const {
+    loading,
     orderForm: { totalizers, value },
   } = useOrderForm()
+
+  const {
+    coupon,
+    insertCoupon,
+    couponErrorKey,
+  } = useOrderCoupon()
 
   return (
     <ExtensionPoint
       id="checkout-summary"
+      loading={loading}
       totalizers={totalizers}
       total={value}
+      coupon={coupon}
+      insertCoupon={insertCoupon}
+      couponErrorKey={couponErrorKey}
     />
   )
 }
 
-export default SummaryWrapper
+const EnhancedSummary = () => (
+  <OrderCouponProvider>
+    <SummaryWrapper />
+  </OrderCouponProvider>
+)
+
+export default EnhancedSummary
