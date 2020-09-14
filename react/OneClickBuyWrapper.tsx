a38094b9eabe7c7f75b3d9e588a8c89d1d0f0ae3
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-09-14 10:26:48 -0300

    Fix prettier

--- react/OneClickBuyWrapper.tsx
+++ react/OneClickBuyWrapper.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ExtensionPoint } from 'vtex.render-runtime'
 
 const OneClickBuyWrapper: StorefrontFunctionComponent = () => (
-    <ExtensionPoint id="one-click-buy" />
+  <ExtensionPoint id="one-click-buy" />
 )
 
 export default OneClickBuyWrapper
